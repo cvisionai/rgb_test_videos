cee65827876b399b6d6deeec8099dfa5713b8aa4
Brian Tate <brian.tate@cvisionai.com>
2021-12-21 16:30:18 -0500

    Make test vidoes longer.

--- generate_test_videos.sh
+++ generate_test_videos.sh
@@ -3,7 +3,7 @@ function make_color_video()
 {
   color=$1
   resolution=$2
-  ffmpeg -y -f lavfi -i nullsrc -filter_complex color=${color}:s=${resolution}x${resolution} -r 30 -frames:v 3000 -c:v libx264 -preset fast -g 25 -movflags faststart+frag_keyframe+empty_moov+default_base_moof -tune fastdecode ${color}.mp4
+  ffmpeg -y -f lavfi -i nullsrc -filter_complex color=${color}:s=${resolution}x${resolution} -r 30 -frames:v 30000 -c:v libx264 -preset fast -g 25 -movflags faststart+frag_keyframe+empty_moov+default_base_moof -tune fastdecode ${color}.mp4
   python3 -m tator.transcode.make_fragment_info -o ${color}.json ${color}.mp4
 }
 
