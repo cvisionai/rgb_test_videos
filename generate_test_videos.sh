bb4171b7793e71f4fa793f065c108634b2be7c19
Brian Tate <brian.tate@cvisionai.com>
2022-01-26 14:00:26 -0500

    Add a 1fps count video.

--- generate_test_videos.sh
+++ generate_test_videos.sh
@@ -16,6 +16,7 @@ ffmpeg -y -f lavfi -i nullsrc -filter_complex "color=white:s=720x720[s];[s]fps=3
 python3 -m tator.transcode.make_fragment_info -o count.json count.mp4
 
 ffmpeg -y -i count.mp4 -vf scale=-2:360 -c:v libx264 -preset fast -g 25 -movflags faststart+frag_keyframe+empty_moov+default_base_moof -tune fastdecode count_360.mp4
+ffmpeg -y -i count.mp4  -c:v libx264 -preset fast -g 25 -movflags faststart+frag_keyframe+empty_moov+default_base_moof -tune fastdecode -r 1 count_1fps.mp4
 python3 -m tator.transcode.make_fragment_info -o count_360.json count_360.mp4
 
 echo "Checking OCR status of video"
